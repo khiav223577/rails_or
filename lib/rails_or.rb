57b8bc5adf2aa54646c961ed11103a1805e1ae31
khiav reoy <mrtmrt15xn@yahoo.com.tw>
2016-12-29 03:14:52 +0800

    fix rails 5 broken

--- lib/rails_or.rb
+++ lib/rails_or.rb
@@ -3,25 +3,30 @@ require 'rails'
 require 'active_record'
 
 class ActiveRecord::Relation
-  def or(other)
-    combining = group_values.any? ? :having : :where
-    left_values = send("#{combining}_values")
-    right_values = other.send("#{combining}_values")
-    common = left_values & right_values
-    mine = left_values - common
-    theirs = right_values - common
-    if mine.any? && theirs.any?
-      mine = mine.map { |x| String === x ? Arel.sql(x) : x }
-      theirs = theirs.map { |x| String === x ? Arel.sql(x) : x }
-      mine = [Arel::Nodes::And.new(mine)] if mine.size > 1
-      theirs = [Arel::Nodes::And.new(theirs)] if theirs.size > 1
-      common << Arel::Nodes::Or.new(mine.first, theirs.first)
+  if method_defined?(:or)
+    alias rails5_or or
+    def or(other)
+      other = self.except(:where).where(other) if other.class == Hash
+      return rails5_or(other)
     end
-    send("#{combining}_values=", common)
-    return self
-    #relation = self.except(:where).where(relation) if relation.class == Hash
-    #left = self.where_clauses.join(' AND ')
-    #right = other.where_clauses.join(' AND ')
-    #return self.except(:where).where("(#{left}) OR (#{right})")
+  else
+    def or(other)
+      other = self.except(:where).where(other) if other.class == Hash
+      combining = group_values.any? ? :having : :where
+      left_values = send("#{combining}_values")
+      right_values = other.send("#{combining}_values")
+      common = left_values & right_values
+      mine = left_values - common
+      theirs = right_values - common
+      if mine.any? && theirs.any?
+        mine = mine.map { |x| String === x ? Arel.sql(x) : x }
+        theirs = theirs.map { |x| String === x ? Arel.sql(x) : x }
+        mine = [Arel::Nodes::And.new(mine)] if mine.size > 1
+        theirs = [Arel::Nodes::And.new(theirs)] if theirs.size > 1
+        common << Arel::Nodes::Or.new(mine.first, theirs.first)
+      end
+      send("#{combining}_values=", common)
+      return self  
+    end
   end
 end
