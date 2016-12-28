8d37335d82d0cb1d363abd871e15137f5fb9d839
khiav reoy <mrtmrt15xn@yahoo.com.tw>
2016-12-29 02:13:37 +0800

    add or query

--- lib/rails_or.rb
+++ lib/rails_or.rb
@@ -2,3 +2,11 @@ require "rails_or/version"
 require 'rails'
 require 'active_record'
 
+class ActiveRecord::Relation
+  def or(relation)
+    relation = self.except(:where).where(relation) if relation.class == Hash
+    left = self.where_clauses.join(' AND ')
+    right = relation.where_clauses.join(' AND ')
+    return self.except(:where).where("(#{left}) OR (#{right})")
+  end
+end
