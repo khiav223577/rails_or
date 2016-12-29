f1ab1edd14c112b709cf46ed0048abfe5370eb86
khiav reoy <mrtmrt15xn@yahoo.com.tw>
2016-12-29 10:27:25 +0800

    support multiple arguments and add test cases

--- test/rails_or_test.rb
+++ test/rails_or_test.rb
@@ -7,6 +7,9 @@ class RailsOrTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::RailsOr::VERSION
   end
+#--------------------------------
+#  Parameter check
+#--------------------------------
   def test_or_with_string_argument
     expected = Post.where('id = 1 or id = 2').to_a
     assert_equal expected, Post.where('id = 1').or('id = 2').to_a
@@ -15,6 +18,28 @@ class RailsOrTest < Minitest::Test
     expected = Post.where('id = 1 or id = 2').to_a
     assert_equal expected, Post.where('id = 1').or(:id => 2).to_a
   end
+  def test_or_with_array_argument
+    expected = Post.where('id = 1 or id = 2').to_a
+    assert_equal expected, Post.where('id = 1').or(['id = ?', 2]).to_a
+  end
+  def test_or_with_multiple_arguments1
+    expected = Post.where('id = ? or id = ?', 1, 2).to_a
+    assert_equal expected, Post.where('id = 1').or('id = ?', 2).to_a
+  end
+  def test_or_with_multiple_arguments2
+    expected = Post.where('id = ? or id = ? or id = ?', 1, 2, 3).to_a
+    assert_equal expected, Post.where('id = 1').or('id = ? OR id = ?', 2, 3).to_a
+    assert_equal expected, Post.where('id = 1').or('id = ?', 2).or('id = ?', 3).to_a
+  end
+#--------------------------------
+#  Multiple columns
+#--------------------------------
+  def test_or_with_multiple_attributes
+    expected = Post.where('id = 1 or title = ?', "Kathenrie's post1").to_a
+    assert_equal expected, Post.where('id = 1').or(:title => "Kathenrie's post1").to_a
+  end
+#--------------------------------
+#  From Rails 5
 #--------------------------------
   def test_or_with_relation
     expected = Post.where('id = 1 or id = 2').to_a
