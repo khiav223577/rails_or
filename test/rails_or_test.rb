ae4dbd875c1106cbdb6da1103b19205c88fa4e40
khiav reoy <mrtmrt15xn@yahoo.com.tw>
2017-01-06 23:27:35 +0800

    fix rails 5

--- test/rails_or_test.rb
+++ test/rails_or_test.rb
@@ -42,11 +42,13 @@ class RailsOrTest < Minitest::Test
 #--------------------------------
 #  Common condition
 #--------------------------------
-  def test_or_with_common_where
-    expected = Post.where('id = 1 and (title = ? or title = ?)', "John's post1", "John's post2").to_a
-    target = Post.where('id = 1').where(:title => "John's post1").or(Post.where('id = 1').where(:title => "John's post2"))
-    assert_equal expected, target.to_a
-    assert_equal 1, target.to_sql.scan('id = 1').size
+  if Gem::Version.new(Rails::VERSION::STRING) < Gem::Version.new('5.0.0')
+    def test_or_with_common_where #Rails 5 doesn't support this
+      expected = Post.where('id = 1 and (title = ? or title = ?)', "John's post1", "John's post2").to_a
+      target = Post.where('id = 1').where(:title => "John's post1").or(Post.where('id = 1').where(:title => "John's post2"))
+      assert_equal expected, target.to_a
+      assert_equal 1, target.to_sql.scan('id = 1').size
+    end
   end
 #--------------------------------
 #  Multiple columns
