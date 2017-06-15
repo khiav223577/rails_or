0283566b18395f27a5ea7002e866fba23300e7f2
khiav reoy <mrtmrt15xn@yahoo.com.tw>
2017-06-15 19:05:18 +0800

    test that Rails 5's native #or implementation doesn't merge same condition

--- test/rails_or_test.rb
+++ test/rails_or_test.rb
@@ -42,14 +42,16 @@ class RailsOrTest < Minitest::Test
 #--------------------------------
 #  Common condition
 #--------------------------------
-  if Gem::Version.new(ActiveRecord::VERSION::STRING) < Gem::Version.new('5.0.0')
-    def test_or_with_common_where #Rails 5 doesn't support this
-      expected = Post.where('id = 1 and (title = ? or title = ?)', "John's post1", "John's post2").to_a
-      target = Post.where('id = 1').where(:title => "John's post1").or(Post.where('id = 1').where(:title => "John's post2"))
-      assert_equal expected, target.to_a
-      assert_equal 1, target.to_sql.scan('id = 1').size
-    end
+  def test_or_with_common_where 
+    expected = Post.where('id = 1 and (title = ? or title = ?)', "John's post1", "John's post2").to_a
+    target = Post.where('id = 1').where(:title => "John's post1").or(Post.where('id = 1').where(:title => "John's post2"))
+    assert_equal expected, target.to_a
+
+    # Rails 5 native #or doesn't merge same condition
+    expected = (Gem::Version.new(ActiveRecord::VERSION::STRING) < Gem::Version.new('5.0.0') ? 1 : 2)
+    assert_equal expected, target.to_sql.scan('id = 1').size
   end
+
   def test_or_with_unneeded_brackets
     #Wrong: SELECT "users".* FROM "users"  WHERE (("users"."id" = 1) OR ("users"."id" = 2))
     #Correct: SELECT "users".* FROM "users"  WHERE ("users"."id" = 1 OR "users"."id" = 2)
