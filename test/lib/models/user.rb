da7cca0ee83a234c9b28483ebac422a647a3f127
khiav reoy <mrtmrt15xn@yahoo.com.tw>
2022-01-22 03:18:21 +0800

    autoload test models

---
+++ test/lib/models/user.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+class User < ActiveRecord::Base
+  serialize :serialized_attribute, Hash
+  has_many :posts
+  has_many :sent_messages,     class_name: 'UserMessage', foreign_key: :sender_user_id,   dependent: :destroy
+  has_many :received_messages, class_name: 'UserMessage', foreign_key: :receiver_user_id, dependent: :destroy
+
+  scope :none, ->{ where('0') } if not User.respond_to?(:none) # For Rails 3
+  if RailsOr::IS_RAILS3_FLAG
+    class << self
+      alias origin_from from if not method_defined?(:origin_from)
+      def from(string)
+        return origin_from if string.is_a?(String)
+        return origin_from("(#{string.to_sql}) subquery") # Rails 3's #from only support string arguments
+      end
+    end
+  end
+end
